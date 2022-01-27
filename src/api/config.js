a43b6812566a2cc3eacc257d0855495e89711505
Chris <chris.a.gonzalez97@gmail.com>
2022-01-26 19:11:30 -0800

    config

---
+++ src/api/config.js
@@ -0,0 +1,3 @@
+export const CLIENTID    ='9lpV5JUz2lcZZCmSN0_SYg';
+export const CLIENTSECRET='Yj4SyrYTPdW6_VfnQ8oT0KXnwAYNeg';
+export const REFRESHTOKEN='1070546588707-E93yrhGR0Dfcj5L6Xxx_jEnuIxN-Lg';
